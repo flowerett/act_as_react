2f24f665a7536d0b3ff92956598f8cf4a19cdfd4
Nick Chernyshev <nick.chernyshev@gmail.com>
2014-12-23 16:45:45 +0500

    fetch comments from data

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,4 +1,10 @@
+var data = [
+  {author: "Pete Hunt", text: "This is **first** comment"},
+  {author: "Jordan Walke", text: "This is *second* comment"}
+];
+
 var converter = new Showdown.converter();
+
 var Comment = React.createClass({
   render: function() {
     var rawMarkup = converter.makeHtml(this.props.children.toString());
@@ -15,10 +21,17 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+          return (
+            <Comment author={comment.author}>
+              {comment.text}
+            </Comment>
+          );
+        });
+
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is **one** comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -39,7 +52,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -47,6 +60,6 @@ var CommentBox = React.createClass({
 });
 
 React.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
