c74b79966f44584b14064354cb31f728d27d753a
Nick Chernyshev <nick.chernyshev@gmail.com>
2014-12-23 16:38:40 +0500

    markdown

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,11 +1,13 @@
+var converter = new Showdown.converter();
 var Comment = React.createClass({
   render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
@@ -15,7 +17,7 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Pete Hunt">This is **one** comment</Comment>
         <Comment author="Jordan Walke">This is *another* comment</Comment>
       </div>
     );
